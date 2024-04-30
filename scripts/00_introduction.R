29f0c062dcb4cdc9231abb80e551d725de934c61
Andrew Bower <a.robert.bower@gmail.com>
2024-04-29 21:52:55 -0700

    Started the project, thru section 1 of chapter 2

---
+++ scripts/00_introduction.R
@@ -0,0 +1,28 @@
+# Description:Introduction to Machine Learning for Social and Behavioral Research
+
+# Author: Andrew R. Bower
+# Version:29 April, 2024
+
+# Packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load("tidyverse", "assertthat", "babynames", "broom", "compare", "DBI",
+"dbplyr", "devtools", "fivethirtyeight", "flexdashboard", "fs", "ggrepel", "googlesheets4",
+"hexbin", "httr", "janitor", "jsonlite", "knitr", "leaflet", "nycflights13", "RPostgreSQL",
+"rvest", "scales", "tidycensus", "usethis", "vroom", "yaml")
+
+# Parameters
+  # Input Data
+
+  # Output Data
+
+  # Load Data
+
+#===============================================================================
+
+#usethis::use_git()
+#usethis::use_github()
+
+
+
+
+
